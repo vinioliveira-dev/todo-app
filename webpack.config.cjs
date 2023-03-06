96fb2b2c3497039acc5e8e27e87e085cf6217af4
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-06 14:17:21 +0100

    feat(webpack): install dependencies and set files to run build for distribution version

---
+++ webpack.config.cjs
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/store.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'store.js',
+        library: 'store',
+        libraryTarget: 'var'
+    }
+};
