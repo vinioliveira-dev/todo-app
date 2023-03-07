c3de8f53327765abd747d0eb9cfad7bda8e5529f
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-07 15:58:27 +0100

    refactor(store): add the new notification action to the store

--- src/todo-store-index.js
+++ src/todo-store-index.js
@@ -3,6 +3,7 @@ export { create } from './todo-store-factory.js';
 
 // Actions
 export { filterSetAction } from './actions/filter-set/filter-set.action.js';
+export { notificationAction } from './actions/notification/notification.action.js';
 export { todosAddAction } from './actions/todos-add/todos-add.action.js';
 export { todosCompletedToggleAction } from './actions/todos-completed-toggle/todos-completed-toggle.action.js';
 export { todosDeleteAction } from './actions/todos-delete/todos-delete.action.js';
