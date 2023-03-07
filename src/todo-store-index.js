435f319b19594545eb46222eee02e9107b29e5ce
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-07 09:54:14 +0100

    refactor(store): add new action to store index and run new build

--- src/todo-store-index.js
+++ src/todo-store-index.js
@@ -4,4 +4,5 @@ export { create } from './todo-store-factory.js';
 // Actions
 export { filtersSetFilterAction } from './actions/filters-set-filter/filters-set-filter.action.js';
 export { todosAddTodoAction } from './actions/todos-add-todo-action/todos-add-todo.action.js';
+export { todosDeleteTodoAction } from './actions/todos-delete-todo/todos-delete-todo.action.js';
 export { todosToggleCompleteAction } from './actions/todos-toggle-complete-action/todos-toggle-complete.action.js';
