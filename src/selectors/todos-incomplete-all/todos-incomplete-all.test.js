7e64f7b303e2e5e45e4162486d9b9860b6708f3f
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-07 15:19:14 +0100

    feat(selector): create the todos-incomplete-all selector

--- src/selectors/todos-incomplete-all/todos-incomplete-all.test.js
+++ src/selectors/todos-incomplete-all/todos-incomplete-all.test.js
@@ -1,13 +1,13 @@
 import _sinon from 'sinon';
 import test from 'tape';
 
-import { todosCompletedAllSelector } from './todos-completed-all.selector.js';
+import { todosIncompleteAllSelector } from './todos-incomplete-all.selector.js';
 
-const TEST_NAME = `todosCompletedAllSelectorModule`;
+const TEST_NAME = `todosIncompleteAllSelectorModule`;
 
 test(TEST_NAME, (t) => {
     t.test(`${TEST_NAME}: `, (t) => {
-        t.equal(typeof todosCompletedAllSelector, 'function', 'the selector should be a function');
+        t.equal(typeof todosIncompleteAllSelector, 'function', 'the selector should be a function');
         t.end();
     });
 
@@ -39,23 +39,23 @@ test(TEST_NAME, (t) => {
         };
 
         const expected = {
-            2: {
-                content: 'string2',
-                completed: true
+            1: {
+                content: 'string1',
+                completed: false
             },
 
-            4: {
-                content: 'string4',
-                completed: true
+            3: {
+                content: 'string3',
+                completed: false
             }
         }
 
-        t.deepEqual(todosCompletedAllSelector(state), expected, 'should return the correct values');
+        t.deepEqual(todosIncompleteAllSelector(state), expected, 'should return the correct values');
         t.end();
     });
 
     t.test(`${TEST_NAME}: if no state object is passed, the selector`, (t) => {
-        t.equal(todosCompletedAllSelector(), undefined, 'should return undefined');
+        t.equal(todosIncompleteAllSelector(), undefined, 'should return undefined');
         t.end();
     });
 });
