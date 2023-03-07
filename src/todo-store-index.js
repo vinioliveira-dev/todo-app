fbf6ebeccacd1b979c7568b2cb60051045da361f
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-07 13:42:36 +0100

    fix(store): fix names for the actions

--- src/todo-store-index.js
+++ src/todo-store-index.js
@@ -2,7 +2,7 @@
 export { create } from './todo-store-factory.js';
 
 // Actions
-export { filtersSetFilterAction } from './actions/filters-set-filter/filters-set-filter.action.js';
-export { todosAddTodoAction } from './actions/todos-add-todo-action/todos-add-todo.action.js';
-export { todosDeleteTodoAction } from './actions/todos-delete-todo/todos-delete-todo.action.js';
-export { todosToggleCompleteAction } from './actions/todos-toggle-complete-action/todos-toggle-complete.action.js';
+export { filterSetAction } from './actions/filter-set/filter-set.action.js';
+export { todosAddAction } from './actions/todos-add/todos-add.action.js';
+export { todosDeleteAction } from './actions/todos-delete/todos-delete.action.js';
+export { todosCompletedToggleAction } from './actions/todos-completed-toggle/todos-completed-toggle.action.js';
