5f3bd3eb6871fc6d915f0af2e6755fb0bccfc060
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-08 09:20:02 +0100

    refactor(store): add new reaction to store configuration

--- src/todo-store-factory.js
+++ src/todo-store-factory.js
@@ -2,15 +2,22 @@
 import { createStore, combineReducers } from 'redux';
 import { combineReactions, createReactionEnhancer } from '../libs/redux-s/redux-s.js';
 
+// custom dependencies for reactions
+import { todosIncompleteAllSelector } from './selectors/todos-incomplete-all/todos-incomplete-all.selector.js';
+
 // reaction
 import { todosDeleteCompletedReaction } from './reactions/todos-delete-completed/todos-delete-completed.reaction.js';
+import { todosCompletedAllNotificationReaction } from './reactions/todos-completed-all-notification/todos-completed-all-notification.reaction.js';
 
 // reducers
 import TODOS_REDUCER, { todosReducer } from './reducers/todos/todos.reducer.js';
 import FILTERS_REDUCER, { filtersReducer } from './reducers/filters/filters.reducer.js';
 
 function create() {
-    const reactions = combineReactions(todosDeleteCompletedReaction);
+    const reactions = combineReactions(
+        todosDeleteCompletedReaction,
+        todosCompletedAllNotificationReaction({ todosIncompleteAllSelector })
+    );
     const reactions_enhancer = createReactionEnhancer();
 
     const reducers = combineReducers({
