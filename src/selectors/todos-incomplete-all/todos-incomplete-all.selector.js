610919218a7d8929199578cd96897d84f53d0c98
Vinícius Queiróz de Oliveira <vqovinicius@gmail.com>
2023-03-09 12:12:29 +0100

    fix(selector): change to concise return statement and return empty array when slice is not present

--- src/selectors/todos-incomplete-all/todos-incomplete-all.selector.js
+++ src/selectors/todos-incomplete-all/todos-incomplete-all.selector.js
@@ -1,18 +1,5 @@
 function todosIncompleteAllSelector(state = {}) {
-    const todos = state?.TODOS_REDUCER?.todos;
-
-    if (!todos) {
-        return undefined;
-    }
-
-    const incompleteTodos = Object.entries(todos || {})
-        .filter(([id, todo]) => !todo.completed)
-        .reduce((completedTodos, [id, todo]) => {
-            completedTodos[id] = todo;
-            return completedTodos;
-        }, {});
-
-    return Object.keys(incompleteTodos).length ? incompleteTodos : 'all to-dos were done';
+    return (state.TODOS?.todos) ? Object.values(state.TODOS.todos).filter(todo => !todo.completed) : [];
 };
 
 export { todosIncompleteAllSelector };
--- src/selectors/todos-incomplete-all/todos-incomplete-all.test.js
+++ src/selectors/todos-incomplete-all/todos-incomplete-all.test.js
@@ -6,31 +6,30 @@ import { todosIncompleteAllSelector } from './todos-incomplete-all.selector.js';
 const TEST_NAME = `todosIncompleteAllSelectorModule`;
 
 test(TEST_NAME, (t) => {
-    t.test(`${TEST_NAME}: `, (t) => {
-        t.equal(typeof todosIncompleteAllSelector, 'function', 'the selector should be a function');
-        t.end();
-    });
-
     t.test(`${TEST_NAME}: the selector`, (t) => {
         const state = {
-            TODOS_REDUCER: {
+            TODOS: {
                 todos: {
                     1: {
+                        id: 1,
                         content: 'string1',
                         completed: false
                     },
 
                     2: {
+                        id: 2,
                         content: 'string2',
                         completed: true
                     },
 
                     3: {
+                        id: 3,
                         content: 'string3',
                         completed: false
                     },
 
                     4: {
+                        id: 4,
                         content: 'string4',
                         completed: true
                     }
@@ -38,42 +37,37 @@ test(TEST_NAME, (t) => {
             }
         };
 
-        const expected = {
-            1: {
-                content: 'string1',
-                completed: false
-            },
-
-            3: {
-                content: 'string3',
-                completed: false
-            }
-        }
-
-        t.deepEqual(todosIncompleteAllSelector(state), expected, 'should return the correct values');
+        const expected = [{ id: 1, content: 'string1', completed: false}, {id: 3, content: 'string3', completed: false }];
+        
+        t.equal(typeof todosIncompleteAllSelector, 'function', 'should be a function');
+        t.deepEqual(todosIncompleteAllSelector(state), expected, 'should return the expected value');
         t.end();
     });
 
     t.test(`${TEST_NAME}: if all "todos" have 'completed: true', the selector`, (t) => {
         const state = {
-            TODOS_REDUCER: {
+            TODOS: {
                 todos: {
                     1: {
+                        id: 1,
                         content: 'string1',
                         completed: true
                     },
 
                     2: {
+                        id: 2,
                         content: 'string2',
                         completed: true
                     },
 
                     3: {
+                        id: 3,
                         content: 'string3',
                         completed: true
                     },
 
                     4: {
+                        id: 4,
                         content: 'string4',
                         completed: true
                     }
@@ -81,14 +75,12 @@ test(TEST_NAME, (t) => {
             }
         };
 
-        const expected = 'all to-dos were done';
-
-        t.deepEqual(todosIncompleteAllSelector(state), expected, 'should return the expected value');
+        t.deepEqual(todosIncompleteAllSelector(state), [], 'should return an empty array');
         t.end();
     });
 
     t.test(`${TEST_NAME}: if no state object is passed, the selector`, (t) => {
-        t.equal(todosIncompleteAllSelector(), undefined, 'should return undefined');
+        t.deepEqual(todosIncompleteAllSelector(), [], 'should return an empty array');
         t.end();
     });
 });
